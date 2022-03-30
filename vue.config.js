0000fe096243693ab221ecf4c59c9b20ff2d1e88
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2022-03-31 12:52:01 +1300

    add baseroute for gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.VUE_APP_BASE_ROUTE || '/',
   transpileDependencies: true,
   chainWebpack: config => {
     config.module
